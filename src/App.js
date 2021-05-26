2fd226a8ca686f0892fe05b6ea3d2344a4cd5a19
royweb <xytank@gmail.com>
2021-05-26 13:16:46 -0400

    Install codemirror and add code editors for HTML, CSS, and Javascript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Editor from './components/Editor';
 
 function App() {
+  const [html, setHTML] = useState('');
+  const [css, setCSS] = useState('');
+  const [js, setJS] = useState('');
+
   return (
     <div >
-      <div className="pane top-pane"></div>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHTML} />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCSS} />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJS} />
+      </div>
       <div className="pane">
         <iframe
           title="output"
